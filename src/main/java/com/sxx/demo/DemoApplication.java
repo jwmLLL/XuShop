ed6b98c509fee76cb4274e53c87522b52f82eee9
LAPTOP-Q14CBR2P\sxx <1379402501@qq.com>
2018-09-13 15:57:39 +0800

    添加 hello 输出

--- src/main/java/com/sxx/demo/DemoApplication.java
+++ src/main/java/com/sxx/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.sxx.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
